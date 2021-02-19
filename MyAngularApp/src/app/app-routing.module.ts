1dfa8cddcdfcc85a8415b03e5efe1dc52791c60a
Tracie Marie <traciemarie@Tracies-MacBook-Air.local>
2021-02-18 20:34:25 -0500

    home page renders

---
+++ MyAngularApp/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
